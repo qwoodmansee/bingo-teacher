a45134e0b8120db0c3ed485de2e2284e41765eb5
DESKTOP-Q1KJN8C\Quinton <qwoodmansee@capital.edu>
2020-06-14 11:08:37 -0400

    added goal search

--- src/components/goal_display.js
+++ src/components/goal_display.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 
 export default function GoalDisplay({goals}) {
+  const [searchValue, setSearchValue] = useState("");
+  
+  const filteredGoals = useCallback(() => {
+    if (searchValue) {
+      return goals.filter((goal) => goal.goalName.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1)
+    } else {
+      return goals;
+    }
+  }, [goals, searchValue]);
   return (
     <div>
-      {goals.map((goal) => {
-        return (
-          <p key={goal.goalName}>
-            {goal.goalName}
-          </p>
-        )
-      })}
+      <div>
+        <h2>Search for goal:</h2>
+        <input 
+          type="text"
+          value={searchValue} 
+          onChange={(e) => {setSearchValue(e.target.value)}}
+        />
+      </div>
+      <div>
+        {filteredGoals().map((goal) => {
+          return (
+            <p key={goal.goalName}>
+              {goal.goalName}
+            </p>
+          )
+        })}
+      </div>
     </div>
   )
 }
