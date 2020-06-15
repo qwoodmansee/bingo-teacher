4aabc53d3219494ddb2ba684b9591a1a0b5de436
Quinton Woodmansee <qwoodmansee@capital.edu>
2020-06-14 22:40:26 -0400

    Updates

---
+++ precache-manifest.18cd21ecd5c5902dc3f9b71049309c43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c77ea09144591bd6b74652d11fb022dc",
+    "url": "/bingo-teacher/index.html"
+  },
+  {
+    "revision": "7bd48e9602f862177a35",
+    "url": "/bingo-teacher/static/css/main.dd569279.chunk.css"
+  },
+  {
+    "revision": "1af48468f92075635607",
+    "url": "/bingo-teacher/static/js/2.479c2c87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bingo-teacher/static/js/2.479c2c87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd48e9602f862177a35",
+    "url": "/bingo-teacher/static/js/main.58f3d038.chunk.js"
+  },
+  {
+    "revision": "edcd034ed35f3857107e",
+    "url": "/bingo-teacher/static/js/runtime-main.d5921fff.js"
+  },
+  {
+    "revision": "66dafda2a3173ae5fbd8e7ce8fd035ad",
+    "url": "/bingo-teacher/static/media/bingo-teacher-icon.66dafda2.png"
+  }
+]);
